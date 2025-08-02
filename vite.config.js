11ba37a4d9b04d01e06c29d3ebd654fea538e967
LyndaLilly <lilianlynda22@gmail.com>
2025-08-02 15:03:54 +0100

    updated

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,20 @@
 /* eslint-disable no-undef */
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path'; // Import the 'path' module
+import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // ✅ Keeps routing working in dev & on Render
   build: {
+    outDir: 'dist', // ✅ Make sure "dist" is set as "Publish Directory" on Render
     rollupOptions: {
       external: ['@react-oauth/google', 'react-qr-code']
-
     }
   },
   resolve: {
     alias: {
-      '@hooks': path.resolve(__dirname, 'src/hooks') // Resolve paths dynamically
-    },
-  },
+      '@hooks': path.resolve(__dirname, 'src/hooks')
+    }
+  }
 });
